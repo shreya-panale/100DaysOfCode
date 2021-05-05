ccf116e60421ff1c29996ed02550205e4615e1eb
shreyapanale <shreya.panale@gmail.com>
2021-05-05 10:13:42 +0530

    HackerRank Day10 Problem Statement and Solution

---
+++ Week2/HackerRank(Day7-13)/Day10_Solution.c
@@ -0,0 +1,147 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+int parse_int(char*);
+
+// My Code
+void solve(int n){
+    int i,size=0,arr[n],count=0,max=0;
+    // arr[] -> binary number in reverse order
+    while(n!=0){
+        i=n%2;
+        n=n/2;
+        arr[size++]=i;
+    }
+    for(i=0;i<size;i++){
+        if(arr[i]==1)
+            count++;
+        // If 0 encountered, check if count is more than max and then reset to 0 for next set of 1's
+        else{
+            if(count>max)
+                max=count;
+            count=0;
+        }
+    }
+    // If arr ends with 1, this if statement is required to verify which set has max 1's
+    if(count>max)
+        max=count;
+    printf("%d",max);
+}
+
+
+int main()
+{
+    int n = parse_int(ltrim(rtrim(readline())));
+    solve(n);
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
